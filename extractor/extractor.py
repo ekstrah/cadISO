81f4230acdc0f51a1f321dd391c92db1332764f2
Dongho Kim <dongho@ekstrah.com>
2023-08-14 16:12:49 +0900

    Remove Thirdparty
    - Removed Thirdparty app

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -1,22 +1,36 @@
-from img2table.ocr import EasyOCR
-from img2table.document import Image
+import cv2
+import pytesseract
+
+import pandas as pd
+
+from utils import Utils
 
 
 class Extractor():
     def __init__(
         self,
     ):
-        self.ocr = EasyOCR(lang=["en"])
+        pass
 
-    def analyze_pdf(self, files):
+    def static_analyzer(self, file):
+        image = cv2.imread(file)
+
+        text = pytesseract.image_to_string(image)
+        rows = [line.split('\t') for line in text.strip().split('\n')]
+        df = pd.DataFrame(rows[1:], columns=rows[0])
+        self.cleansing_result(df)
+        output_csv = 'output_data.csv'
+        df.to_csv(output_csv, index=False)
+
+        print(f"CSV file saved as {output_csv}")
+
+    def cleansing_result(self, df):
+        for id, row in df.iterrows():
+            print(id, row)
+
+
+    def analyze_static_all(self, src_path):
+        files = Utils.get_all_files(src_path)
         for file in files:
-            doc = Image(file)
-            print(file)
-            doc.to_xlsx(
-                dest="./result.xlsx",
-                ocr=self.ocr,
-                implicit_rows=False,
-                borderless_tables=False,
-                min_confidence=90,
-            )
+            self.static_analyzer(file)
             return
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from utils import Utils
 if __name__ == "__main__":
     e = Extractor()
     print("extractor imported")
-    e.analyze_pdf(Utils.get_all_files("./src/tables"))
+    e.analyze_static_all("./src/tables")
