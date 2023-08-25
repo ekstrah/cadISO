7739155f242a48a2a5834789ab184d81890b7cf5
Dongho Kim <dongho@ekstrah.com>
2023-08-25 19:45:26 +0900

    Update
    Update Files

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from extractor import Extractor
 if __name__ == "__main__":
     e = Extractor()
     Utils.init_subdirs()
-    Pdfer.convert_all_pdfs()
-    # for file in files:
-    #     cells = e.extract_cells(file, ".png")
-    #     print(file)
-    #     e.to_json(cells, file.replace("tables", "results").replace(".png", ".json"))
+    for file in files:
+        cells = e.extract_cells(file, ".png")
+        print(file)
+        e.to_json(cells, file.replace("tables", "results").replace(".png", ".json"))
     print("Done")
--- taslice/taslice.py
+++ taslice/taslice.py
@@ -14,9 +14,14 @@ def get_all_files(src_path, extension):
 table_engine = PPStructure(table=False, ocr=False, show_log=True)
 save_folder = "./output"
 
-files = get_all_files("../src/img")
+files = get_all_files("../src/img", ".jpg")
 for file in files:
     img_path = file
     img = cv2.imread(img_path)
     result = table_engine(img)
     save_structure_res(result, save_folder, os.path.basename(img_path).split('.')[0])
+
+    for line in result:
+        line.pop('img')
+        print(line)
+    break
